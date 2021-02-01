d979698a650b1f20ed8e57f8d683df3abadbe9b6
DanilMazurkin <danilmazurkin27@gmail.com>
2021-02-01 12:03:41 +1000

    First Views Contrroler

--- learndjango/urls.py
+++ learndjango/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('', include('djangoapp.urls')),
+    # url('admin/', admin.site.urls),
 ]
